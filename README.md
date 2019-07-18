14bdc52797f30718308b9d82d215b6c242c626e1
Molan Ren <ren.molan@gmail.com>
2019-07-17 19:00:43 -0700

    Add interaction between covariates

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# advertising-linear-regression
+# sales-linear-regression
+
+Creating a multiple linear regression model in order to predict sales. 
+
+Data retrieved from [An Introduction to Statistical Learning](http://faculty.marshall.usc.edu/gareth-james/ISL/data.html).
+
+## Data
+
+### advertising.csv 
+
+| Column | Description |
+| --- | --- |
+| `TV` | TV advertising budget (thousands of dollars) |
+| `Radio` | Radio advertising budget (thousands of dollars) |
+| `Newspaper` | Newspaper advertising budget (thousands of dollars) |
+| `Sales` | Number of sales (thousands of units) |
--- main.R
+++ main.R
@@ -6,20 +6,21 @@ summary(df)
 # Drop index column
 df <- subset(df, select = -X)
 
-# Exploratory plots
-pairs(~.,data=df, main="Scatterplot Matrix")
-# plots of tv/radio/newspaper show random scatter -> no correlation
-# sales vs TV/radio/newspaper shows linear relationship
-# sales vs newspaper shows fan-shape -> indicates heteroskedasticity 
-
-# Create train and test sets (60/40 split)
-sample_size <- floor(0.6 * nrow(df))
+# Create train and test sets (70/30 split)
+sample_size <- floor(0.7 * nrow(df))
 set.seed(100)
 train_index <- sample(seq_len(nrow(df)), size = sample_size)
 
 train <- df[train_index, ]
 test <- df[-train_index, ]
 
+# Exploratory plots
+pairs(~.,data=train, main="Scatterplot Matrix")
+# plots of tv/radio/newspaper show random scatter -> no correlation
+# sales vs TV/radio/newspaper shows linear relationship -> let's try using a linear model
+# sales vs newspaper and sales vs TV show fan-shape -> indicates heteroskedasticity 
+
+# Selecting a linear model
 # Choose a linear model using backward elimination
 # Fit model with all explanatory variables                      
 fullmodel <- lm(sales~.,data=df)
@@ -28,6 +29,8 @@ summary(fullmodel)
 # Drop newspaper
 updatedmodel <- update(fullmodel, .~.-newspaper)
 summary(updatedmodel)
+
+# All explanatory variables have p-value > alpha, so we stop backward elimination here
 
 # Choose a linear model using all subsets regression
 library(leaps)
@@ -51,8 +54,38 @@ plot(2:4, summary(best.subset)$adjr2, xlab="No. of Parameters",
      ylab="Adjusted R-squared", col="blue")
 
 # From Cp values and adjusted R2 values, the 2 explanatory variable model looks best (TV and radio)
+# This matches the linear model chosen using backwards elimination
+
+# Visually assess how well the linear model fits the data
+plot(updatedmodel)
+# Residuals vs fitted plots shows quadratic relationship -> indicates non-linearity (need higher order polynomial terms)
+#   or other explanatory variables are not captured in the model
+# QQ-plot is S-shaped -> indicates residuals are not normally distributed 
 
 # Make predictions for test data
 predicted <- predict(updatedmodel, test)
 prediction_results <- data.frame(cbind(actual=test$sales, predicted=predicted))  
 correlation_accuracy <- cor(prediction_results) # 93.5%
+
+# Adding interaction 
+# The Residuals vs fitted plot had a quadratic relationship -> could be result of explanatory variables
+#   not captured in the model
+# Try adding interaction between the two explanatory variables and see if it is significant
+model_with_interaction <- lm(sales ~ TV + radio + TV * radio, data = train)
+summary(model_with_interaction)
+# p-value of TV:radio interaction is less than alpha -> interaction is significant given other variables
+#   in the model
+
+# Visually assess how well the linear model with interaction fits the data
+plot(model_with_interaction)
+# Residuals vs fitted plot shows less of a quadratic relation than model without interaction
+# QQ-plot is U-shaped -> transformation of response may help with normality of residuals
+
+# Make new predictions
+predicted2 <- predict(model_with_interaction, test)
+prediction_results2 <- data.frame(cbind(actual=test$sales, predicted=predicted2))  
+correlation_accuracy2 <- cor(prediction_results2) # 97.8%
+
+
+
+
